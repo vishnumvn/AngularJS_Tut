63f3449a2326db12310c3122909c3e0b2fcda5c0
vishnu <vishnumvn@gmail.com>
2018-05-22 17:47:50 +0530

    menu and customer component

---
+++ Day2/Angular4/QuickStart/src/app/show-customer/show-customer.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowCustomerComponent } from './show-customer.component';
+
+describe('ShowCustomerComponent', () => {
+  let component: ShowCustomerComponent;
+  let fixture: ComponentFixture<ShowCustomerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ShowCustomerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShowCustomerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Day2/Angular4/QuickStart/src/app/show-customer/show-customer.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from 'src/app/customer';
+
+@Component({
+  selector: 'app-show-customer',
+  templateUrl: './show-customer.component.html',
+  styleUrls: ['./show-customer.component.css']
+})
+export class ShowCustomerComponent implements OnInit {
+
+   p1: Customer;
+   p2: Customer;
+  constructor()
+  {
+    this.p1 = {'customerId': 11, 'customerName': 'P1_name', 'image': '../assets/images/p1.png' };
+    this.p2 = {'customerId': 22, 'customerName': 'P2_name', 'image': '../assets/images/p2.png' };
+  }
+
+  ngOnInit() {
+  }
+
+}
