76a5ce0e0d8e2a44990d19bee8801b827ab4106d
vishnu <vishnumvn@gmail.com>
2018-05-22 16:16:46 +0530

    first angular app

---
+++ Day2/Angular4/QuickStart/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent] // One component used to start while booting
+})
+export class AppModule { }
